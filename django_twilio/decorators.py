6dc05f69b2def3533dd1932748e298bd33d2fe96
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2013-10-12 16:05:09 +0200

    remove unused imports

--- django_twilio/decorators.py
+++ django_twilio/decorators.py
@@ -5,7 +5,6 @@ from functools import wraps
 
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 from django.http import (HttpRequest, HttpResponse, HttpResponseForbidden,
     HttpResponseNotAllowed)
 
